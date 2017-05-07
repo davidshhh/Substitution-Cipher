f8f8e26f222b604c9c83fc78824a31e50eb65ea5
Jiawei Huang <jhujhuang@gmail.com>
2017-05-07 01:49:18 -0400

    have cipher.py able to take output path

--- cipher.py
+++ cipher.py
@@ -6,7 +6,7 @@ from pprint import pprint
 '''
 
 
-def getKey():
+def get_key():
     key = {}
     mapping = [chr(i) for i in range(ord('a'), ord('z') + 1)]
     for i in range(ord('a'), ord('z') + 1):
@@ -21,30 +21,32 @@ def getKey():
 '''
 
 
-def cipher(plaintext_filename, length):
-    key = getKey()
+def cipher(plaintext_filename, outfile, length):
+    key = get_key()
     f = open(plaintext_filename, 'r')
-    cipher = open('A-cipher.txt', 'w')
+    out = open(outfile, 'w')
     count = 0
     for l in f:
         for c in l:
-            cipher.write(key[c])
+            out.write(key[c])
             count += 1
             if c == ' ' and count >= length:
                 pprint(key)
-                cipher.close()
+                out.close()
                 return
     pprint(key)
-    cipher.close()
+    out.close()
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("plaintext_filename")
+parser.add_argument("output_filename")
 parser.add_argument("--length", default=6000)
 
 args = parser.parse_args()
 
-plaintext_filename = args.plaintext_filename
-length = args.length
+pf = args.plaintext_filename
+o = args.output_filename
+l = args.length
 
-cipher(plaintext_filename, length)
+cipher(pf, o, l)
